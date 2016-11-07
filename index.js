c4dade1c1a3b2c312e50f75a972ee0bb34fa4d6f
John Otander <johnotander@gmail.com>
2016-11-07 13:20:30 -0700

    Move trie creation into index

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 'use strict'
 
 const globby = require('globby')
-const { createTrie } = require('./trie')
+const trie = require('wayfarer/trie')
 
 module.exports = (glob, opts = {}) => {
   const options = Object.assign({}, { nodir: true }, opts)
   return globby(glob, options).then(createTrie)
+}
+
+const createTrie = files => {
+  console.log(files)
+
+  const fileTrie = trie()
+  const firstFile = files.shift()
+  const node = fileTrie.create(firstFile)
+  files.forEach(f => fileTrie.mount(f, fileTrie))
+  return trie
 }
